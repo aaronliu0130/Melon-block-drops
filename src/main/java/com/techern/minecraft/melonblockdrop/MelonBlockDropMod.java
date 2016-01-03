69c43b5989bf5e7d82a44331473587d3914dcc0d
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-01-04 09:03:16 +1000

    Add crafting recipes for melon blocks

--- src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
+++ src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
@@ -1,10 +1,12 @@
 package com.techern.minecraft.melonblockdrop;
 
-import net.minecraft.block.BlockMelon;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -41,6 +43,40 @@ public class MelonBlockDropMod {
         LOGGER = event.getModLog();
 
         LOGGER.info("Set up MelonBlockDrop's logging");
+    }
+
+    /**
+     * Handles the {@link FMLInitializationEvent}
+     *
+     * @param event The {@link FMLInitializationEvent}
+     * @since 1.0.0
+     */
+    @Mod.EventHandler
+    public void handleInit(FMLInitializationEvent event) {
+
+        registerMelonBlockCraftingRecipes();
+
+        LOGGER.info("Set melon block crafting recipes");
+    }
+
+    /**
+     * Registers the melon block crafting recipes
+     *
+     * @since 0.0.1
+     */
+    public void registerMelonBlockCraftingRecipes() {
+
+        ItemStack block = new ItemStack(Blocks.melon_block, 1);
+
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 2), block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 6), block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 9), block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 16), block, block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 25), block, block, block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 36), block, block, block, block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 42), block, block, block, block, block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 48), block, block, block, block, block, block, block, block);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.melon, 63), block, block, block, block, block, block, block, block, block);
     }
 
 }
