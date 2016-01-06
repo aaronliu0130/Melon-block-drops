e415d987421776f12a21bf25e58458418c92cab9
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-01-07 08:22:18 +1000

    Drop back to 1.8 and release 1.1.0

--- src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
+++ src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
@@ -24,7 +24,7 @@ public class MelonBlockDropMod {
      *
      * @since 1.0.0
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     /**
      * The {@link Logger} for {@link MelonBlockDropMod}
