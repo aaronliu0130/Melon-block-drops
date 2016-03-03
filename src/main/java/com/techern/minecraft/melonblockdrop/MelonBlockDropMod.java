a8c4a024000dcc885ee7d978455eab2662e78d0f
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-03-03 15:21:34 +1000

    Release version 1.2.0

--- src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
+++ src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
@@ -23,7 +23,7 @@ public class MelonBlockDropMod {
      *
      * @since 1.0.0
      */
-    public static final String VERSION = "1.2.0-SNAPSHOT";
+    public static final String VERSION = "1.2.0";
 
     /**
      * The {@link Logger} for {@link MelonBlockDropMod}
