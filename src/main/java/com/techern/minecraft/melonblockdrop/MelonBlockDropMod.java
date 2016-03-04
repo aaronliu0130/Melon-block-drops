863f736ffbd2b3c35db2ec2df69b0f71250878a3
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-03-04 22:04:51 +1000

    Try for 1.2.1

--- src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
+++ src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
@@ -23,7 +23,7 @@ public class MelonBlockDropMod {
      *
      * @since 1.0.0
      */
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
 
     /**
      * The {@link Logger} for {@link MelonBlockDropMod}
