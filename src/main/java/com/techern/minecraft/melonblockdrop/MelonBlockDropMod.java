f10c4eac3a86f7c961d79219d52ff900d645c11b
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-04-13 14:34:21 +1000

    Release v1.3

--- src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
+++ src/main/java/com/techern/minecraft/melonblockdrop/MelonBlockDropMod.java
@@ -23,7 +23,7 @@ public class MelonBlockDropMod {
      *
      * @since 1.0.0
      */
-    public static final String VERSION = "1.3-SNAPSHOT";
+    public static final String VERSION = "1.3";
 
     /**
      * The {@link Logger} for {@link MelonBlockDropMod}
